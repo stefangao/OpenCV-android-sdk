3c79383395e95a1246ddd9306242db6073b3fcb0
Stefan <stefan.gao@gmail.com>
2019-05-31 17:36:22 +0800

    add colorization and ageGendor

--- samples/face-detection/jni/NativeProcessor.cpp
+++ samples/face-detection/jni/NativeProcessor.cpp
@@ -28,9 +28,9 @@ inline void vector_Rect_to_Mat(vector<Rect>& v_rect, Mat& mat)
     mat = Mat(v_rect, true);
 }
 
-///////////////// native call hanlder ////////////////////
+///////////////// native call handler ////////////////////
 
-static long hanlder_convertToGray(cob::ValueMap& params)
+static long handler_convertToGray(cob::ValueMap& params)
 {
     long srcAddr = params["src"].asLong();
     long dstAddr = params["dst"].asLong();
@@ -43,7 +43,7 @@ static long hanlder_convertToGray(cob::ValueMap& params)
     return 0;
 }
 
-static long hanlder_drawRect(cob::ValueMap& params)
+static long handler_drawRect(cob::ValueMap& params)
 {
     long srcAddr = params["src"].asLong();
     long dstAddr = params["dst"].asLong();
@@ -63,7 +63,7 @@ static long hanlder_drawRect(cob::ValueMap& params)
     return 0;
 }
 
-static long hanlder_detectBlob(cob::ValueMap& params)
+static long handler_detectBlob(cob::ValueMap& params)
 {
     long srcAddr = params["src"].asLong();
     long dstAddr = params["dst"].asLong();
@@ -244,7 +244,7 @@ static void splitByLine(std::vector<char>& content, vector<string>& ret)
 }
 
 
-static long hanlder_configNet(cob::ValueMap& params)
+static long handler_configNet(cob::ValueMap& params)
 {
     auto cfgAddr = params["cfg"].asLong();
     auto modelAddr = params["model"].asLong();
@@ -262,7 +262,7 @@ static long hanlder_configNet(cob::ValueMap& params)
         std::vector<char> vecClass = (vector<char>)(classMat);
         splitByLine(vecClass, classes);
 
-        predictNet = readNetFromDarknet(vecCfg, vecModel);
+        predictNet = dnn::readNetFromDarknet(vecCfg, vecModel);
         predictNet.setPreferableBackend(DNN_BACKEND_OPENCV);
         predictNet.setPreferableTarget(DNN_TARGET_CPU);
     }
@@ -270,7 +270,7 @@ static long hanlder_configNet(cob::ValueMap& params)
 }
 
 
-static long hanlder_detectObject(cob::ValueMap& params)
+static long handler_detectObject(cob::ValueMap& params)
 {
     long srcAddr = params["src"].asLong();
     long dstAddr = params["dst"].asLong();
@@ -306,7 +306,7 @@ static long hanlder_detectObject(cob::ValueMap& params)
     return 0;
 }
 
-static long hanlder_processCloak(cob::ValueMap& params)
+static long handler_processCloak(cob::ValueMap& params)
 {
     long srcAddr = params["src"].asLong();
     long dstAddr = params["dst"].asLong();
@@ -342,13 +342,253 @@ static long hanlder_processCloak(cob::ValueMap& params)
     return 0;
 }
 
+// the 313 ab cluster centers from pts_in_hull.npy (already transposed)
+static float hull_pts[] = {
+        -90., -90., -90., -90., -90., -80., -80., -80., -80., -80., -80., -80., -80., -70., -70., -70., -70., -70., -70., -70., -70.,
+        -70., -70., -60., -60., -60., -60., -60., -60., -60., -60., -60., -60., -60., -60., -50., -50., -50., -50., -50., -50., -50., -50.,
+        -50., -50., -50., -50., -50., -50., -40., -40., -40., -40., -40., -40., -40., -40., -40., -40., -40., -40., -40., -40., -40., -30.,
+        -30., -30., -30., -30., -30., -30., -30., -30., -30., -30., -30., -30., -30., -30., -30., -20., -20., -20., -20., -20., -20., -20.,
+        -20., -20., -20., -20., -20., -20., -20., -20., -20., -10., -10., -10., -10., -10., -10., -10., -10., -10., -10., -10., -10., -10.,
+        -10., -10., -10., -10., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 10., 10., 10., 10., 10., 10., 10.,
+        10., 10., 10., 10., 10., 10., 10., 10., 10., 10., 10., 20., 20., 20., 20., 20., 20., 20., 20., 20., 20., 20., 20., 20., 20., 20.,
+        20., 20., 20., 30., 30., 30., 30., 30., 30., 30., 30., 30., 30., 30., 30., 30., 30., 30., 30., 30., 30., 30., 40., 40., 40., 40.,
+        40., 40., 40., 40., 40., 40., 40., 40., 40., 40., 40., 40., 40., 40., 40., 40., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50.,
+        50., 50., 50., 50., 50., 50., 50., 50., 50., 60., 60., 60., 60., 60., 60., 60., 60., 60., 60., 60., 60., 60., 60., 60., 60., 60.,
+        60., 60., 60., 70., 70., 70., 70., 70., 70., 70., 70., 70., 70., 70., 70., 70., 70., 70., 70., 70., 70., 70., 70., 80., 80., 80.,
+        80., 80., 80., 80., 80., 80., 80., 80., 80., 80., 80., 80., 80., 80., 80., 80., 90., 90., 90., 90., 90., 90., 90., 90., 90., 90.,
+        90., 90., 90., 90., 90., 90., 90., 90., 90., 100., 100., 100., 100., 100., 100., 100., 100., 100., 100., 50., 60., 70., 80., 90.,
+        20., 30., 40., 50., 60., 70., 80., 90., 0., 10., 20., 30., 40., 50., 60., 70., 80., 90., -20., -10., 0., 10., 20., 30., 40., 50.,
+        60., 70., 80., 90., -30., -20., -10., 0., 10., 20., 30., 40., 50., 60., 70., 80., 90., 100., -40., -30., -20., -10., 0., 10., 20.,
+        30., 40., 50., 60., 70., 80., 90., 100., -50., -40., -30., -20., -10., 0., 10., 20., 30., 40., 50., 60., 70., 80., 90., 100., -50.,
+        -40., -30., -20., -10., 0., 10., 20., 30., 40., 50., 60., 70., 80., 90., 100., -60., -50., -40., -30., -20., -10., 0., 10., 20.,
+        30., 40., 50., 60., 70., 80., 90., 100., -70., -60., -50., -40., -30., -20., -10., 0., 10., 20., 30., 40., 50., 60., 70., 80., 90.,
+        100., -80., -70., -60., -50., -40., -30., -20., -10., 0., 10., 20., 30., 40., 50., 60., 70., 80., 90., -80., -70., -60., -50.,
+        -40., -30., -20., -10., 0., 10., 20., 30., 40., 50., 60., 70., 80., 90., -90., -80., -70., -60., -50., -40., -30., -20., -10.,
+        0., 10., 20., 30., 40., 50., 60., 70., 80., 90., -100., -90., -80., -70., -60., -50., -40., -30., -20., -10., 0., 10., 20., 30.,
+        40., 50., 60., 70., 80., 90., -100., -90., -80., -70., -60., -50., -40., -30., -20., -10., 0., 10., 20., 30., 40., 50., 60., 70.,
+        80., -110., -100., -90., -80., -70., -60., -50., -40., -30., -20., -10., 0., 10., 20., 30., 40., 50., 60., 70., 80., -110., -100.,
+        -90., -80., -70., -60., -50., -40., -30., -20., -10., 0., 10., 20., 30., 40., 50., 60., 70., 80., -110., -100., -90., -80., -70.,
+        -60., -50., -40., -30., -20., -10., 0., 10., 20., 30., 40., 50., 60., 70., -110., -100., -90., -80., -70., -60., -50., -40., -30.,
+        -20., -10., 0., 10., 20., 30., 40., 50., 60., 70., -90., -80., -70., -60., -50., -40., -30., -20., -10., 0.
+};
+
+static Net colorization_net;
+
+static long handler_colorization_configNet(cob::ValueMap& params)
+{
+    auto cfgAddr = params["cfg"].asLong();
+    auto modelAddr = params["model"].asLong();
+    if (cfgAddr != 0 && modelAddr != 0)
+    {
+        Mat &cfgMat = *((Mat *) cfgAddr);
+        Mat &modelMat = *((Mat *) modelAddr);
+
+        // Load the network config
+        std::vector<uchar> vecCfg = (vector<uchar>)(cfgMat);
+        std::vector<uchar> vecModel = (vector<uchar>)(modelMat);
+
+        colorization_net = dnn::readNetFromCaffe(vecCfg, vecModel);
+
+        // setup additional layers:
+        int sz[] = {2, 313, 1, 1};
+        const Mat pts_in_hull(4, sz, CV_32F, hull_pts);
+        Ptr<dnn::Layer> class8_ab = colorization_net.getLayer("class8_ab");
+        class8_ab->blobs.push_back(pts_in_hull);
+        Ptr<dnn::Layer> conv8_313_rh = colorization_net.getLayer("conv8_313_rh");
+        conv8_313_rh->blobs.push_back(Mat(1, 313, CV_32F, Scalar(2.606)));
+    }
+    return 0;
+}
+
+static long handler_colorize(cob::ValueMap& params)
+{
+    const int W_in = 224;
+    const int H_in = 224;
+
+    long srcAddr = params["src"].asLong();
+    long dstAddr = params["dst"].asLong();
+    if (srcAddr != 0 && dstAddr != 0)
+    {
+        Mat &srcMat = *((Mat *) srcAddr);
+        Mat &dstMat = *((Mat *) dstAddr);
+        Mat frame = srcMat;
+
+        // extract L channel and subtract mean
+        Mat lab, L, input;
+        frame.convertTo(frame, CV_32F, 1.0/255);
+        cvtColor(frame, lab, COLOR_RGB2Lab);
+        extractChannel(lab, L, 0);
+        resize(L, input, Size(W_in, H_in));
+        input -= 50;
+
+        // run the L channel through the network
+        Mat inputBlob = blobFromImage(input);
+        colorization_net.setInput(inputBlob);
+        Mat result = colorization_net.forward();
+
+        // retrieve the calculated a,b channels from the network output
+        Size siz(result.size[2], result.size[3]);
+        Mat a = Mat(siz, CV_32F, result.ptr(0,0));
+        Mat b = Mat(siz, CV_32F, result.ptr(0,1));
+        resize(a, a, frame.size());
+        resize(b, b, frame.size());
+
+        // merge, and convert back to BGR
+        Mat &color = dstMat;
+        Mat chn[] = {L, a, b};
+        merge(chn, 3, lab);
+        cvtColor(lab, color, COLOR_Lab2RGB);
+
+        color = color*255;
+        color.convertTo(color, CV_8U);
+    }
+    return 0;
+}
+
+//// age gender ////
+static Net ag_ageNet;
+static Net ag_genderNet;
+static Net ag_faceNet;
+
+static long handler_ageGender_configNet(cob::ValueMap& params)
+{
+    auto& cfgArray = params["cfg"].asValueVector();
+    auto& modelArray = params["model"].asValueVector();
+    if (cfgArray.size() == 3 && modelArray.size() == 3)
+    {
+        Mat &ageCfg = *((Mat *) cfgArray[0].asLong());
+        Mat &genderCfg = *((Mat *) cfgArray[1].asLong());
+        Mat &faceCfg = *((Mat *) cfgArray[2].asLong());
+        Mat &ageModel = *((Mat *) modelArray[0].asLong());
+        Mat &genderModel = *((Mat *) modelArray[1].asLong());
+        Mat &faceModel = *((Mat *) modelArray[2].asLong());
+
+        // Load Network
+        ag_ageNet = dnn::readNetFromCaffe((vector<uchar>)(ageCfg), (vector<uchar>)(ageModel));
+        ag_genderNet = dnn::readNetFromCaffe((vector<uchar>)(genderCfg), (vector<uchar>)(genderModel));
+        ag_faceNet = dnn::readNetFromTensorflow((vector<uchar>)(faceModel), (vector<uchar>)(faceCfg));
+    }
+    return 0;
+}
+
+tuple<Mat, vector<vector<int>>> getFaceBox(Net net, Mat &frame, double conf_threshold)
+{
+    Mat frameOpenCVDNN = frame.clone();
+    int frameHeight = frameOpenCVDNN.rows;
+    int frameWidth = frameOpenCVDNN.cols;
+    double inScaleFactor = 1.0;
+    Size size = Size(300, 300);
+    // std::vector<int> meanVal = {104, 117, 123};
+    Scalar meanVal = Scalar(104, 117, 123);
+
+    cv::Mat inputBlob;
+    //inputBlob = cv::dnn::blobFromImage(frameOpenCVDNN, inScaleFactor, size, meanVal, true, false);
+    inputBlob = cv::dnn::blobFromImage(frameOpenCVDNN, inScaleFactor, cv::Size(frameWidth, frameHeight), meanVal, true, false);
+
+    net.setInput(inputBlob, "data");
+    cv::Mat detection = net.forward("detection_out");
+
+    cv::Mat detectionMat(detection.size[2], detection.size[3], CV_32F, detection.ptr<float>());
+
+    vector<vector<int>> bboxes;
+
+    for(int i = 0; i < detectionMat.rows; i++)
+    {
+        float confidence = detectionMat.at<float>(i, 2);
+
+        if(confidence > conf_threshold)
+        {
+            int x1 = static_cast<int>(detectionMat.at<float>(i, 3) * frameWidth);
+            int y1 = static_cast<int>(detectionMat.at<float>(i, 4) * frameHeight);
+            int x2 = static_cast<int>(detectionMat.at<float>(i, 5) * frameWidth);
+            int y2 = static_cast<int>(detectionMat.at<float>(i, 6) * frameHeight);
+            vector<int> box = {x1, y1, x2, y2};
+            bboxes.push_back(box);
+            cv::rectangle(frameOpenCVDNN, cv::Point(x1, y1), cv::Point(x2, y2), cv::Scalar(0, 255, 0),2, 4);
+        }
+    }
+
+    return make_tuple(frameOpenCVDNN, bboxes);
+}
+
+static long handler_detectAgeGender(cob::ValueMap& params)
+{
+    const int padding = 20;
+    Scalar MODEL_MEAN_VALUES = Scalar(78.4263377603, 87.7689143744, 114.895847746);
+    vector<string> ageList = {"(0-2)", "(4-6)", "(8-12)", "(15-20)", "(25-32)",
+                              "(38-43)", "(48-53)", "(60-100)"};
+
+    vector<string> genderList = {"Male", "Female"};
+
+    long srcAddr = params["src"].asLong();
+    long dstAddr = params["dst"].asLong();
+    if (srcAddr != 0 && dstAddr != 0)
+    {
+        Mat &srcMat = *((Mat *) srcAddr);
+        Mat &dstMat = *((Mat *) dstAddr);
+        Mat frame = srcMat;
+
+        vector<vector<int>> bboxes;
+        Mat frameFace;
+        tie(frameFace, bboxes) = getFaceBox(ag_faceNet, frame, 0.7);
+
+        if(bboxes.size() > 0)
+        {
+            for (auto it = begin(bboxes); it != end(bboxes); ++it)
+            {
+                Rect rec(it->at(0) - padding, it->at(1) - padding,
+                         it->at(2) - it->at(0) + 2 * padding,
+                         it->at(3) - it->at(1) + 2 * padding);
+                Mat face = frame(rec); // take the ROI of box on the frame
+
+                Mat blob;
+                blob = blobFromImage(face, 1, Size(227, 227), MODEL_MEAN_VALUES, false);
+                ag_genderNet.setInput(blob);
+                // string gender_preds;
+                vector<float> genderPreds = ag_genderNet.forward();
+                // printing gender here
+                // find max element index
+                // distance function does the argmax() work in C++
+                int max_index_gender = std::distance(genderPreds.begin(),
+                                                     max_element(genderPreds.begin(),
+                                                                 genderPreds.end()));
+                string gender = genderList[max_index_gender];
+
+                ag_ageNet.setInput(blob);
+                vector<float> agePreds = ag_ageNet.forward();
+                /* // uncomment below code if you want to iterate through the age_preds
+                 * vector
+                cout << "PRINTING AGE_PREDS" << endl;
+                for(auto it = age_preds.begin(); it != age_preds.end(); ++it) {
+                  cout << *it << endl;
+                }
+                */
+
+                // finding maximum indicd in the age_preds vector
+                int max_indice_age = std::distance(agePreds.begin(),
+                                                   max_element(agePreds.begin(), agePreds.end()));
+                string age = ageList[max_indice_age];
+                string label = gender + ", " + age; // label
+                cv::putText(frameFace, label, Point(it->at(0), it->at(1) - 15),
+                            cv::FONT_HERSHEY_SIMPLEX, 0.9, Scalar(0, 255, 255), 2, cv::LINE_AA);
+            }
+        }
+    }
+    return 0;
+}
+
+
 static std::unordered_map<std::string, std::function<long(cob::ValueMap&)>> mNativeCallHandlerMap {
-    {"cvt2Gray", hanlder_convertToGray},
-    {"drawRect", hanlder_drawRect},
-    {"detectBlob", hanlder_detectBlob},
-    {"configNet", hanlder_configNet},
-    {"detectObject", hanlder_detectObject},
-    {"processCloak", hanlder_processCloak},
+    {"cvt2Gray", handler_convertToGray},
+    {"drawRect", handler_drawRect},
+    {"detectBlob", handler_detectBlob},
+    {"configNet", handler_configNet},
+    {"detectObject", handler_detectObject},
+    {"processCloak", handler_processCloak},
+    {"colorization_configNet", handler_colorization_configNet},
+    {"colorize", handler_colorize},
+    {"ageGender_configNet", handler_ageGender_configNet},
+    {"detectAgeGender", handler_detectAgeGender},
 };
 
 JNIEXPORT long JNICALL Java_org_opencv_samples_facedetect_DetectionBasedTracker_nativeCallCxx(JNIEnv*  env, jclass thiz, jstring jstrFunc, jstring jstrParam)
